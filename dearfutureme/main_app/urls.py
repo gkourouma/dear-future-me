df1a5e5fbf57e7d056c44df01b9075b9d7df3908
gkourouma <gkourouma2@gmail.com>
2025-05-01 08:04:06 -0500

    WIP: Added path for capsule page and create capsule page

--- dearfutureme/main_app/urls.py
+++ dearfutureme/main_app/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('capsules/', views.capsule_page, name='capsules'),
+    path('capsule/create/', views.capsule_create, name='capsule_create'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
