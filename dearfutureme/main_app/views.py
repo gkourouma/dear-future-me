3d26ac167fbd05cd2eda06270e6bac53bc50e0ec
gkourouma <gkourouma2@gmail.com>
2025-05-01 08:09:54 -0500

    WIP: Refactored capsule views. Renamed and implemented capsule creation logic

--- dearfutureme/main_app/views.py
+++ dearfutureme/main_app/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .forms import CapsuleForm
+from .models import Capsule
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
 
-def capsules(request):
-    return render(request, 'capsules.html')
+def capsule_page(request):
+    capsules = Capsule.objects.filter(user=request.user)
+    return render(request, 'capsules.html', {'capsules': capsules})
+
+def capsule_create(request):
+    if request.method == 'POST':
+        form = CapsuleForm(request.POST, request.FILES)
+        if form.is_valid():
+            capsule = form.save(commit=False)
+            capsule.user = request.user
+            capsule.save()
+            return redirect('capsules')
+    else:
+        form = CapsuleForm()
+    return render(request, 'capsule_form.html', {'form': form})
 
 
 def signup_view(request):
@@ -18,10 +33,11 @@ def signup_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')
+            return redirect('capsules')
         else:
             error_message = 'Opp! There was an error with your signup. Please try again.'
-    form = UserCreationForm()
+    else:
+        form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'signup.html', context)
 
@@ -31,7 +47,7 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('home')
+            return redirect('capsules')
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
