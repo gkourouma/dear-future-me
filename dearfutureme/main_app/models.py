e0d03a00d762a7473e48864ea52ed6b60ae4e91b
gkourouma <gkourouma2@gmail.com>
2025-05-01 08:08:25 -0500

    WIP: Added Capsule model with fields for user, title, content, cover image, and dates

--- dearfutureme/main_app/models.py
+++ dearfutureme/main_app/models.py
@@ -2,3 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Capsule(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_locked = models.BooleanField(default=False)
+    cover_image = models.ImageField(upload_to='capsule_images/', blank=True, null=True)
+    open_date = models.DateField()
+
+    def __str__(self):
+        return self.title
