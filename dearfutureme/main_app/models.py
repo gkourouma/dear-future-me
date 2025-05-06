8528ba9a753759da11fe286c8da4744e7da83141
gkourouma <gkourouma2@gmail.com>
2025-05-05 22:20:11 -0500

    Added related_name to user ForeignKey in Capsule and Memory models

--- dearfutureme/main_app/models.py
+++ dearfutureme/main_app/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Capsule(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='capsules')
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -16,7 +16,7 @@ class Capsule(models.Model):
     
 class Memory(models.Model):
     capsule = models.ForeignKey(Capsule, on_delete=models.CASCADE, related_name='memories')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='memories')
     title = models.CharField(max_length=255)
     date_taken = models.DateField(null=True, blank=True)
     content = models.TextField()
@@ -26,4 +26,12 @@ class Memory(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'Memory for {self.capsule.title} - {self.text[:20]}'
+        return f'Memory for {self.capsule.title} - {self.content[:20]}'
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
