0d3cc1f259ddae3aee41d254cf7ca36d0b548e24
gkourouma <gkourouma2@gmail.com>
2025-05-07 11:23:36 -0500

    Added user albums and memory interaction routes to URLs

--- dearfutureme/main_app/urls.py
+++ dearfutureme/main_app/urls.py
@@ -18,10 +18,14 @@ urlpatterns = [
     path('capsule/<int:capsule_id>/delete/', views.capsule_delete, name='capsule_delete'),
     path('capsule/<int:capsule_id>/edit/', views.capsule_edit, name='capsule_edit'),
     path('capsule/<int:capsule_id>/', views.capsule_detail, name='capsule_detail'),
+    path('users/<int:user_id>/albums/', views.user_capsules, name='user_capsules'),
     path('capsules/<int:capsule_id>/add-memory/', views.add_memory, name='memory_form'),
     path('memories/<int:memory_id>/', views.memory_detail, name='memory_detail'),
     path('memories/<int:memory_id>/delete/', views.memory_delete, name='memory_delete'),
     path('memories/<int:memory_id>/edit/', views.memory_edit, name='memory_edit'),
+    path('memory/<int:memory_id>/like/', views.like_memory, name='like_memory'),
+    path('memory/<int:memory_id>/comment/', views.add_comment, name='add_comment'),
+
 ]
 
 if settings.DEBUG:
