a737a73407b9e069efd5c2f5ca3394c9fc00b12a
gkourouma <gkourouma2@gmail.com>
2025-05-07 17:17:18 -0500

    Removed MEDIA_ROOT setting from Django settings

--- dearfutureme/settings.py
+++ dearfutureme/settings.py
@@ -150,7 +150,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media uploaded by users
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
